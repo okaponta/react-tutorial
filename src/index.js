330d2dcd31f93ce95db2e7de747c855f77aca506
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-07-09 01:53:56 +0900

    format

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0 , this.state.stepNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -92,7 +92,7 @@ class Game extends React.Component {
         </li>
       );
     });
-    
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
